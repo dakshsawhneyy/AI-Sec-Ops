433c885a318e6f14aab329b563cf8b7bedb3aaf9
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-21 15:21:23 +0530

    AKS Infra created using IAC

---
+++ infrastructure/azure/outputs.tf
@@ -0,0 +1,11 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.demo.name
+}
+
+output "aks_cluster_nodes" {
+  value = azurerm_kubernetes_cluster.demo.default_node_pool
+}
+
+output "Change_Config_Command" {
+  value = "az aks get-credentials --resource-group ${azurerm_resource_group.demo.name} --name ${azurerm_kubernetes_cluster.demo.name}"
+}
---
+++ infrastructure/azure/variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+  default = "ai-sec-ops"
+}
+
+variable "location" {
+  default = "Central India"
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
