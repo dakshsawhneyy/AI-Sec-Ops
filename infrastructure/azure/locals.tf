0abc9d8904723176bbac8f2bf3e430e2f6f8b7b2
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-26 12:47:15 +0530

    AWS Infra initialized inside azure using remote S3

--- infrastructure/azure/locals.tf
+++ infrastructure/azure/locals.tf
@@ -1,4 +1,20 @@
+# Capturing AWS Created Infrastructure from its bucket and using them in Azure
+data "terraform_remote_state" "aws" {
+  backend = "s3"
+  config = {
+    bucket = "aiops-platform-sf"
+    key    = "aws/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
 locals {
+
+  falco_access_key = data.terraform_remote_state.aws.outputs.falco_access_key
+  falco_secret_key = data.terraform_remote_state.aws.outputs.falco_secret_key
+  kinesis_stream_name = data.terraform_remote_state.aws.outputs.kinesis_stream_name
+  log_group_name = data.terraform_remote_state.aws.outputs.log_group_name
+
   common_tags = {
     Project     = var.project_name
     ManagedBy   = "terraform"
